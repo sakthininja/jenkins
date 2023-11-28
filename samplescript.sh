93bb46699072308dc948c17b2f4172820a774ee8
sakthininja <sakthirojatwins@gmail.com>
2023-11-28 09:41:10 -0800

    script file

---
+++ samplescript.sh
@@ -0,0 +1,19 @@
+set -e
+mkdir script
+cd script
+touch file{1..5}.html
+echo " <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html> " >> index.html
+cat index.html
+echo " my bash script successfully worked"
+
